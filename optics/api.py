8392207ed594da1d81673f9c19d10d81dec88ac3
ashish-greycube <mr.ashish.shah@gmail.com>
2020-02-06 15:12:45 +0530

    pass template_item_code

--- optics/api.py
+++ optics/api.py
@@ -5,4 +5,4 @@ from erpnext.portal.product_configurator.utils import get_item_codes_by_attribut
 def get_item_codes_by_attributes_cf(attribute_filters, template_item_code=None):
 	attribute_filters=frappe.parse_json(frappe.form_dict.attribute_filters)	
 	print('-----------------------------------',attribute_filters)
-	return get_item_codes_by_attributes(attribute_filters, template_item_code=None)
+	return get_item_codes_by_attributes(attribute_filters, template_item_code)
