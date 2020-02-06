4d6b31696a7b44070fc9390e83e5f1a2bb37e4c7
ashish-greycube <mr.ashish.shah@gmail.com>
2020-02-06 13:04:23 +0530

    determine lens type based on attribute

--- optics/public/js/sales_invoice.js
+++ optics/public/js/sales_invoice.js
@@ -77,193 +77,258 @@ frappe.ui.form.on('Sales Invoice', {
 			frm.doc.items=[]
 			frm.refresh_field('items')
 		}		
+//  decide lens type
+let right_lens_type='';
+frappe.call({
+	method: "erpnext.portal.product_configurator.utils.get_attributes_and_values",
+	args: {
+		item_code: right_lens_cf
+	},
+	callback: function (r) {
+		if (!r.exc) {
+			console.log('r.message.length',r.message.length)
+			if (r.message.length==4) {
+				right_lens_type='ByFocal'
+			}
+			else if(r.message.length==2){
+				right_lens_type= 'Maxima'
+			}else{
+				frappe.throw(__('Something went wrong..lens type couldn\'t be determined.'));
+			}
+			if (right_lens_type == 'ByFocal') {
+				if (r_sph_1 == undefined || r_cyl_1 == undefined || r_sph_2 == undefined || r_cyl_2 == undefined) {
+					frappe.msgprint(__('For ByFocal, please enter distance and reading values for right lens'));
+					return false
+				} else {
+					var items = {
+						'SPHD': r_sph_1,
+						'CYLD': r_cyl_1,
+						'SPHR': r_sph_2,
+						'CYLR': r_cyl_2
+					};
+					console.log(1)
+					frappe.call({
+						method: "optics.api.get_item_codes_by_attributes_cf",
+						args: {
+							attribute_filters: items,
+							template_item_code: right_lens_cf,
+						},
+						callback: function (r) {
+							if (!r.exc) {
+								var item_code = r.message[0]
+								if (item_code) {
+									var child = frm.add_child("items");
+									frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
+									frm.refresh_field("items")
+								} else {
+									frappe.msgprint(__('No ByFocal item found for these values of right lens '))
+								}
+							}
+						}
+					});
+				}
+			}
+			if (right_lens_type == 'Maxima' || right_lens_type == 'Altima') {
+				if ((r_sph_1 != undefined && r_cyl_1 == undefined) || (r_sph_1 == undefined && r_cyl_1 != undefined)) {
+					frappe.msgprint(__('For {0}, right lense please enter both SPH and CYL distance values', [right_lens_cf]));
+					return false
+				} 
+				// distance
+				if (r_sph_1 != undefined && r_cyl_1 != undefined){
+						var items = {
+							'SPH': r_sph_1,
+							'CYL': r_cyl_1,
+						};
+						console.log(2)
+					frappe.call({
+						method: "optics.api.get_item_codes_by_attributes_cf",
+						args: {
+							attribute_filters: items,
+							template_item_code: right_lens_cf,
+						},
+						callback: function (r) {
+							if (!r.exc) {
+								var item_code = r.message[0]
+								if (item_code) {
+									var child = frm.add_child("items");
+									frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
+									frm.refresh_field("items")
+								} else {
+									frappe.msgprint(__('No {0} item found for distance values of right lens ', [right_lens_cf]))
+								}
+							}
+						}
+					});
+				}
+				}
+			
+			if (right_lens_type == 'Maxima' || right_lens_type == 'Altima') {
+				 if ((r_sph_2 != undefined && r_cyl_2 == undefined) || (r_sph_2 == undefined && r_cyl_2 != undefined)) {
+					frappe.msgprint(__('For {0}, right lense please enter both SPH and CYL reading values', [right_lens_cf]));
+					return false
+				} 
+				if (r_sph_2 != undefined && r_cyl_2 != undefined){
+						//  reading
+						var items = {
+							'SPH': r_sph_2,
+							'CYL': r_cyl_2,
+						};
+						console.log(3)
+					frappe.call({
+						method: "optics.api.get_item_codes_by_attributes_cf",
+						args: {
+							attribute_filters: items,
+							template_item_code: right_lens_cf,
+						},
+						callback: function (r) {
+							if (!r.exc) {
+								var item_code = r.message[0]
+								if (item_code) {
+									var child = frm.add_child("items");
+									frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
+									frm.refresh_field("items")
+								} else {
+									frappe.msgprint(__('No {0} item found for reading values of right lens ', [right_lens_cf]))
+								}
+							}
+						}
+					});
+				}
 
-		if (right_lens_cf == 'ByFocal') {
-			if (r_sph_1 == undefined || r_cyl_1 == undefined || r_sph_2 == undefined || r_cyl_2 == undefined) {
-				frappe.msgprint(__('For ByFocal, please enter distance and reading values for right lens'));
-				return false
-			} else {
-				var items = {
-					'SPHD': r_sph_1,
-					'CYLD': r_cyl_1,
-					'SPHR': r_sph_2,
-					'CYLR': r_cyl_2
-				};
-				frappe.call({
-					method: "optics.api.get_item_codes_by_attributes_cf",
-					args: {
-						attribute_filters: items,
-						template_item_code: right_lens_cf,
-					},
-					callback: function (r) {
-						if (!r.exc) {
-							var item_code = r.message[0]
-							if (item_code) {
-								var child = frm.add_child("items");
-								frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
-								frm.refresh_field("items")
-							} else {
-								frappe.msgprint(__('No ByFocal item found for these values of right lens '))
-							}
-						}
-					}
-				});
-			}
-		} 
-		if (left_lens_cf == 'ByFocal') {
-			if (l_sph_1 == undefined || l_cyl_1 == undefined || l_sph_2 == undefined || l_cyl_2 == undefined) {
-				frappe.msgprint(__('For ByFocal, please enter distance and reading values for left lens'));
-				return false
-			} else {
-				var items = {
-					'SPHD': l_sph_1,
-					'CYLD': l_cyl_1,
-					'SPHR': l_sph_2,
-					'CYLR': l_cyl_2
-				};
-				frappe.call({
-					method: "optics.api.get_item_codes_by_attributes_cf",
-					args: {
-						attribute_filters: items,
-						template_item_code: left_lens_cf,
-					},
-					callback: function (r) {
-						if (!r.exc) {
-							var item_code = r.message[0]
-							if (item_code) {
-								var child = frm.add_child("items");
-								frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
-								frm.refresh_field("items")
-							} else {
-								frappe.msgprint(__('No ByFocal item found for these values of left lens '))
-							}
-						}
-					}
-				});
-			}
-		} 
-		if (right_lens_cf == 'Maxima' || right_lens_cf == 'Altima') {
-			if ((r_sph_1 != undefined && r_cyl_1 == undefined) || (r_sph_1 == undefined && r_cyl_1 != undefined)) {
-				frappe.msgprint(__('For {0}, right lense please enter both SPH and CYL distance values', [right_lens_cf]));
-				return false
-			} 
-					var items = {
-						'SPH': r_sph_1,
-						'CYL': r_cyl_1,
-					};
-				frappe.call({
-					method: "optics.api.get_item_codes_by_attributes_cf",
-					args: {
-						attribute_filters: items,
-						template_item_code: right_lens_cf,
-					},
-					callback: function (r) {
-						if (!r.exc) {
-							var item_code = r.message[0]
-							if (item_code) {
-								var child = frm.add_child("items");
-								frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
-								frm.refresh_field("items")
-							} else {
-								frappe.msgprint(__('No {0} item found for distance values of right lens ', [right_lens_cf]))
-							}
-						}
-					}
-				});
-			}
-		
-		if (right_lens_cf == 'Maxima' || right_lens_cf == 'Altima') {
-			 if ((r_sph_2 != undefined && r_cyl_2 == undefined) || (r_sph_2 == undefined && r_cyl_2 != undefined)) {
-				frappe.msgprint(__('For {0}, right lense please enter both SPH and CYL reading values', [right_lens_cf]));
-				return false
-			} 
-					//  reading
-					var items = {
-						'SPH': r_sph_2,
-						'CYL': r_cyl_2,
-					};
-				frappe.call({
-					method: "optics.api.get_item_codes_by_attributes_cf",
-					args: {
-						attribute_filters: items,
-						template_item_code: right_lens_cf,
-					},
-					callback: function (r) {
-						if (!r.exc) {
-							var item_code = r.message[0]
-							if (item_code) {
-								var child = frm.add_child("items");
-								frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
-								frm.refresh_field("items")
-							} else {
-								frappe.msgprint(__('No {0} item found for reading values of right lens ', [right_lens_cf]))
-							}
-						}
-					}
-				});
-			}
-		if (left_lens_cf == 'Maxima' || left_lens_cf == 'Altima') {
-			if ((l_sph_1 != undefined && l_cyl_1 == undefined) || (l_sph_1 == undefined && l_cyl_1 != undefined)) {
-				frappe.msgprint(__('For {0}, left lense please enter both SPH and CYL distance values', [left_lens_cf]));
-				return false
-			} 
-					// distance
-					var items = {
-						'SPH': l_sph_1,
-						'CYL': l_cyl_1,
-					};
+				}
+		}
+	}
+});
 
-				frappe.call({
-					method: "optics.api.get_item_codes_by_attributes_cf",
-					args: {
-						attribute_filters: items,
-						template_item_code: left_lens_cf,
-					},
-					callback: function (r) {
-						if (!r.exc) {
-							var item_code = r.message[0]
-							if (item_code) {
-								var child = frm.add_child("items");
-								frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
-								frm.refresh_field("items")
-							} else {
-								frappe.msgprint(__('No {0} item found for distance values of left lens ', [left_lens_cf]))
-							}
-						}
-					}
-				});
+let left_lens_type='';
+frappe.call({
+	method: "erpnext.portal.product_configurator.utils.get_attributes_and_values",
+	args: {
+		item_code: left_lens_cf
+	},
+	callback: function (r) {
+		if (!r.exc) {
+			console.log('r.message.length',r.message.length)
+			if (r.message.length==4) {
+				left_lens_type='ByFocal'
 			}
-		
-		if (left_lens_cf == 'Maxima' || left_lens_cf == 'Altima') {
-		 if ((l_sph_2 != undefined && l_cyl_2 == undefined) || (l_sph_2 == undefined && l_cyl_2 != undefined)) {
-				frappe.msgprint(__('For {0}, left lense please enter both SPH and CYL reading values', [left_lens_cf]));
-				return false
-			} 
-					//  reading
+			else if(r.message.length==2){
+				left_lens_type= 'Maxima'
+			}else{
+				frappe.throw(__('Something went wrong..lens type couldn\'t be determined.'));
+			}
+			if (left_lens_type == 'ByFocal') {
+				if (l_sph_1 == undefined || l_cyl_1 == undefined || l_sph_2 == undefined || l_cyl_2 == undefined) {
+					frappe.msgprint(__('For ByFocal, please enter distance and reading values for left lens'));
+					return false
+				} else {
 					var items = {
-						'SPH': l_sph_2,
-						'CYL': l_cyl_2,
+						'SPHD': l_sph_1,
+						'CYLD': l_cyl_1,
+						'SPHR': l_sph_2,
+						'CYLR': l_cyl_2
 					};
-				frappe.call({
-					method: "optics.api.get_item_codes_by_attributes_cf",
-					args: {
-						attribute_filters: items,
-						template_item_code: left_lens_cf,
-					},
-					callback: function (r) {
-						if (!r.exc) {
-							var item_code = r.message[0]
-							if (item_code) {
-								var child = frm.add_child("items");
-								frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
-								frm.refresh_field("items")
-							} else {
-								frappe.msgprint(__('No {0} item found for these values of left lens ', [left_lens_cf]))
+					console.log(4)
+					frappe.call({
+						method: "optics.api.get_item_codes_by_attributes_cf",
+						args: {
+							attribute_filters: items,
+							template_item_code: left_lens_cf,
+						},
+						callback: function (r) {
+							if (!r.exc) {
+								var item_code = r.message[0]
+								if (item_code) {
+									var child = frm.add_child("items");
+									frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
+									frm.refresh_field("items")
+								} else {
+									frappe.msgprint(__('No ByFocal item found for these values of left lens '))
+								}
 							}
 						}
-					}
-				});
-			}
-		}		
+					});
+				}
+			} 
+	
+			if (left_lens_type == 'Maxima' || left_lens_type == 'Altima') {
+				if ((l_sph_1 != undefined && l_cyl_1 == undefined) || (l_sph_1 == undefined && l_cyl_1 != undefined)) {
+					frappe.msgprint(__('For {0}, left lense please enter both SPH and CYL distance values', [left_lens_cf]));
+					return false
+				} 
+				if (l_sph_1 != undefined && l_cyl_1 != undefined)	{
+				// distance
+						var items = {
+							'SPH': l_sph_1,
+							'CYL': l_cyl_1,
+						};
+						console.log(5)
+					frappe.call({
+						method: "optics.api.get_item_codes_by_attributes_cf",
+						args: {
+							attribute_filters: items,
+							template_item_code: left_lens_cf,
+						},
+						callback: function (r) {
+							if (!r.exc) {
+								var item_code = r.message[0]
+								if (item_code) {
+									var child = frm.add_child("items");
+									frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
+									frm.refresh_field("items")
+								} else {
+									frappe.msgprint(__('No {0} item found for distance values of left lens ', [left_lens_cf]))
+								}
+							}
+						}
+					});
+				}
+				}
+
+				if (left_lens_type == 'Maxima' || left_lens_type == 'Altima') {
+					if ((l_sph_2 != undefined && l_cyl_2 == undefined) || (l_sph_2 == undefined && l_cyl_2 != undefined)) {
+						   frappe.msgprint(__('For {0}, left lense please enter both SPH and CYL reading values', [left_lens_cf]));
+						   return false
+					   } 
+					   if (l_sph_2 != undefined && l_cyl_2 != undefined) {
+					   //  reading
+							   var items = {
+								   'SPH': l_sph_2,
+								   'CYL': l_cyl_2,
+							   };
+							   console.log(6)
+						   frappe.call({
+							   method: "optics.api.get_item_codes_by_attributes_cf",
+							   args: {
+								   attribute_filters: items,
+								   template_item_code: left_lens_cf,
+							   },
+							   callback: function (r) {
+								   if (!r.exc) {
+									   var item_code = r.message[0]
+									   if (item_code) {
+										   var child = frm.add_child("items");
+										   frappe.model.set_value(child.doctype, child.name, "item_code", item_code)
+										   frm.refresh_field("items")
+									   } else {
+										   frappe.msgprint(__('No {0} item found for these values of left lens ', [left_lens_cf]))
+									   }
+								   }
+							   }
+						   });
+						}	
+						
+						}
+
+		}}});
+
+
+
+ 
+
+		
+
+		},
+		
 	
 })
